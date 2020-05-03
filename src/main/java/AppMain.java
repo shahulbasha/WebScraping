1c7b2455c1b60cb95873418b11b9ee863279dac9
ShahulBasha <shhl.basha333@gmail.com>
2020-05-03 20:31:50 +0530

    removed unused imports

--- src/main/java/AppMain.java
+++ src/main/java/AppMain.java
@@ -1,9 +1,5 @@
 import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.google.gson.stream.JsonReader;
-import jdk.nashorn.internal.objects.Global;
-import jdk.nashorn.internal.parser.JSONParser;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -19,7 +15,6 @@ public static void main(String[] args){
     try {
         Document doc=Jsoup.connect("https://www.jiosaavn.com/featured/weekly-top-songs").get();
         Elements elements = doc.select("ol.track-list");
-       // System.out.println(elements.select("div.song-json").first().text());
         Elements elem = elements.select("div.song-json");
         for (Element element : elem) {
             JsonElement json = JsonParser.parseString(element.text());
